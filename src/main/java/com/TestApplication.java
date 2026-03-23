bcd3f55c3e11c41d9b2b3c1ff3fac3544dd40557
org-spring <org.springframework.boot@gmail.com>
2026-03-23 16:27:26 +0530

    complete

--- src/main/java/com/TestApplication.java
+++ src/main/java/com/TestApplication.java
@@ -2,10 +2,18 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class TestApplication {
 
+    @GetMapping("/")
+    public String greet() {
+        return "hello world!";
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
     }
